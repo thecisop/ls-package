5551958562085fbe412163c03660da5b747deb07
TimC0328 <TimC0328@gmail.com>
2021-03-23 16:04:29 -0400

    If player tries to spawn solider outside of cast range, soldieris spawned at closest point within cast range

--- Content/LeagueSandbox-Scripts/Champions/Azir/W.cs
+++ Content/LeagueSandbox-Scripts/Champions/Azir/W.cs
@@ -35,57 +35,50 @@ namespace Spells
         public void OnFinishCasting(IObjAiBase owner, ISpell spell, IAttackableUnit target)
         {
             var castrange = spell.SpellData.CastRange[0];
-            var apbonus = owner.Stats.AbilityPower.Total * 0.2f;
+            var apbonus = owner.Stats.AbilityPower.Total * 0.6f;
             var damage = 35 + ((15 * (spell.Level - 1)) + apbonus); //TODO: Should replace minion AA damage
             var jackduration = 5.0f; //TODO: Split into Active duration and Hidden duration when Invisibility is implemented
-            var attspeed = 1 / 1.8f; // 1.8 attacks a second = ~.56 seconds per attack, could not extrapolate from minion stats
-            //TODO: Implement Fear buff and ShacoBoxSpell
-            //var fearrange = 300;
-            var fearduration = 0.5f + (0.25 * (spell.Level - 1));
+            var attspeed = 1 / (owner.Stats.AttackSpeedFlat * .55f);
             var ownerPos = owner.Position;
             var spellPos = new Vector2(spell.X, spell.Y);
 
-            if (Extensions.IsVectorWithinRange(ownerPos, spellPos, castrange))
+            if (!Extensions.IsVectorWithinRange(ownerPos, spellPos, castrange))
             {
-                IMinion m = AddMinion((IChampion)owner, "AzirSoldier", "AzirSoldier", spellPos);
-                AddParticle(owner, "JackintheboxPoof.troy", spellPos);
+                spellPos = Extensions.GetClosestCircleEdgePoint(spellPos, ownerPos, castrange);
+            }
 
-                var attackrange = m.Stats.Range.Total;
+            IMinion m = AddMinion((IChampion)owner, "AzirSoldier", "AzirSoldier", spellPos);
+            // AddParticle(owner, "JackintheboxPoof.troy", spellPos);
 
-                if (m.IsVisibleByTeam(owner.Team))
+            var attackrange = m.Stats.Range.Total;
+
+            if (!m.IsDead)
+            {
+                var units = GetUnitsInRange(m.Position, attackrange, true);
+                foreach (var value in units)
                 {
-                    if (!m.IsDead)
+                    if (owner.Team != value.Team && value is IAttackableUnit && !(value is IBaseTurret) && !(value is IObjAnimatedBuilding))
                     {
-                        var units = GetUnitsInRange(m.Position, attackrange, true);
-                        foreach (var value in units)
+                        m.SetTargetUnit(value);
+                        m.AutoAttackProjectileSpeed = 1450;
+                        m.AutoAttackHit(value);
+                        for (petTimeAlive = 0.0f; petTimeAlive < jackduration; petTimeAlive += attspeed)
                         {
-                            if (owner.Team != value.Team && value is IAttackableUnit && !(value is IBaseTurret) && !(value is IObjAnimatedBuilding))
+                            CreateTimer(petTimeAlive, () =>
                             {
-                                //TODO: Change TakeDamage to activate on Jack AutoAttackHit, not use CreateTimer, and make Pets use owner spell stats
-                                m.SetTargetUnit(value);
-                                m.AutoAttackProjectileSpeed = 1450;
-                                m.AutoAttackHit(value);
-                                for (petTimeAlive = 0.0f; petTimeAlive < jackduration; petTimeAlive += attspeed)
+                                if (!value.IsDead && !m.IsDead)
                                 {
-                                    CreateTimer(petTimeAlive, () => {
-                                        if (!value.IsDead && !m.IsDead)
-                                        {
-                                            value.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
-                                        }
-                                    });
+                                    value.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
                                 }
-                            }
+                            });
                         }
-                        CreateTimer(jackduration, () =>
-                        {
-                            if (!m.IsDead)
-                            {
-                                m.Die(m); //TODO: Fix targeting issues
-                            }
-                        });
                     }
+
                 }
             }
+
+
+
         }
 
         public void ApplyEffects(IObjAiBase owner, IAttackableUnit target, ISpell spell, IProjectile projectile)
